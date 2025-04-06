388398912ee2665a1715c6684e1655ee5da2666a
Matheus Henrique Moreira <matheushmoreira695@gmail.com>
2025-04-06 17:11:23 -0300

    Página home dinâmica

---
+++ public/app.js
@@ -0,0 +1,89 @@
+const dados = [
+    {
+      "id": 1,
+      "titulo": "Bífe de boi",
+      "descricao": "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis inventore, ipsa ratione pariatur nostrum totam dignissimos voluptatum laboriosam distinctio beatae.",
+      "Ingredientes": "",
+      "categoria": "Carnes",
+      "autor": "Claudia",
+      "data": "06/04/2025",
+      "imagem": "imagens/prato1"
+    },
+
+    {
+        "id": 2,
+        "titulo": "Macarrão",
+        "descricao": "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis inventore, ipsa ratione pariatur nostrum totam dignissimos voluptatum laboriosam distinctio beatae.",
+        "Ingredientes": "",
+        "categoria": "Massas",
+        "autor": "Claudia",
+        "data": "06/04/2025",
+        "imagem": "imagens/prato2"
+      },
+
+      {
+        "id": 3,
+        "titulo": "Salada",
+        "descricao": "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis inventore, ipsa ratione pariatur nostrum totam dignissimos voluptatum laboriosam distinctio beatae.",
+        "Ingredientes": "",
+        "categoria": "Saudável",
+        "autor": "Claudia",
+        "data": "06/04/2025",
+        "imagem": "imagens/prato3"
+      },
+
+      {
+        "id": 4,
+        "titulo": "Nachos",
+        "descricao": "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis inventore, ipsa ratione pariatur nostrum totam dignissimos voluptatum laboriosam distinctio beatae.",
+        "Ingredientes": "",
+        "categoria": "Rápidas",
+        "autor": "Claudia",
+        "data": "06/04/2025",
+        "imagem": "imagens/prato4"
+      },
+
+      {
+        "id": 5,
+        "titulo": "Espetinho",
+        "descricao": "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis inventore, ipsa ratione pariatur nostrum totam dignissimos voluptatum laboriosam distinctio beatae.",
+        "Ingredientes": "",
+        "categoria": "Peixes",
+        "autor": "Claudia",
+        "data": "06/04/2025",
+        "imagem": "imagens/prato5"
+      },
+
+      {
+        "id": 6,
+        "titulo": "Frango ao molho tarê",
+        "descricao": "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis inventore, ipsa ratione pariatur nostrum totam dignissimos voluptatum laboriosam distinctio beatae.",
+        "Ingredientes": "",
+        "categoria": "Carnes",
+        "autor": "Claudia",
+        "data": "06/04/2025",
+        "imagem": "imagens/prato6"
+      },
+
+      {
+        "id": 7,
+        "titulo": "Frango frito",
+        "descricao": "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis inventore, ipsa ratione pariatur nostrum totam dignissimos voluptatum laboriosam distinctio beatae.",
+        "Ingredientes": "",
+        "categoria": "Carnes",
+        "autor": "Claudia",
+        "data": "06/04/2025",
+        "imagem": "imagens/prato7"
+      },
+
+      {
+        "id": 8,
+        "titulo": "Bolinho de azeitona",
+        "descricao": "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis inventore, ipsa ratione pariatur nostrum totam dignissimos voluptatum laboriosam distinctio beatae.",
+        "ingredientes": "",
+        "categoria": "Receitas da vovó",
+        "autor": "Claudia",
+        "data": "06/04/2025",
+        "imagem": "imagens/prato8"
+      }
+]
